a7330443ac9c7505a2511e073e80ca0309bf0949
memojpinkus <memojpinkus@gmail.com>
2022-05-03 13:51:52 -0700

    Updated allowed hosts

---
+++ posts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
+
+# Create your views here.
+class PostListView(ListView):
+    template_name = 'posts/list.html'
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = 'posts/detail.html'
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = 'posts/new.html'
+    model = Post
+    fields = ['title', 'body']
